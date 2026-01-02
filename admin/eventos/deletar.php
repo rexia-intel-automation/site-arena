7133fbb9708ff3ae8a787ce9b7d4e19d305df0e2
rexia-intel-automation <mohamad@rexia.com.br>
2026-01-02 19:00:51 -0300

    Update deletar.php

--- admin/eventos/deletar.php
+++ admin/eventos/deletar.php
@@ -65,6 +65,7 @@ $csrfToken = gerarCSRFToken();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?> - Arena BRB Admin</title>
+    <link rel="stylesheet" href="assets/css/design-system.css">
     <link rel="stylesheet" href="/admin/assets/css/admin.css">
     <link rel="stylesheet" href="/admin/assets/css/admin-minimal.css">
 </head>
