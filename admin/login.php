cd7642c452db479b901a7899023e7f171ca57b7c
rexia-intel-automation <mohamad@rexia.com.br>
2026-01-02 18:08:22 -0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Arena BRB Admin</title>
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="/admin/assets/login.css">
 </head>
 <body>
     <div class="login-container">
