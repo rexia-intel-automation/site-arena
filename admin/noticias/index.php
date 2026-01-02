5d86b001457567c926c84d29417040f7ffb15ac2
rexia-intel-automation <mohamad@rexia.com.br>
2026-01-02 19:02:22 -0300

    Update index.php

--- admin/noticias/index.php
+++ admin/noticias/index.php
@@ -78,6 +78,7 @@ if (isset($_GET['erro'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?> - Arena BRB Admin</title>
+    <link rel="stylesheet" href="assets/css/design-system.css">
     <link rel="stylesheet" href="/admin/assets/css/admin.css">
     <link rel="stylesheet" href="/admin/assets/css/admin-minimal.css">
 </head>
